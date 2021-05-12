48b886453e8ffbd930910650c0129def1c16015e
wangyingjun <277933360@qq.com>
2021-05-13 07:25:20 +0800

    InsertSort

--- code/sort.js
+++ code/sort.js
@@ -30,6 +30,7 @@ const BubbleSort = (sourceArray) => {
 let arr = [5,4,2,3,8]
 console.log(BubbleSort(arr))
 
+// 选择排序
 // 首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置
 // 再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
 // 重复第二步，直到所有元素均排序完毕。
@@ -57,3 +58,31 @@ const SelectionSort = (sourceArray) => {
 }
 
 console.log(SelectionSort(arr))
+
+// 插入排序
+// 将第一待排序序列第一个元素看做一个有序序列，把第二个元素到最后一个元素当成是未排序序列。
+// 从头到尾依次扫描未排序序列，将扫描到的每个元素插入有序序列的适当位置。（如果待插入的元素与有序序列中的某个元素相等，则将待插入元素插入到相等元素的后面。）
+ 
+const InsertSort = (sourceArray) => {
+    if(!Array.isArray(sourceArray) || !sourceArray.length) return sourceArray;
+    const arr = [...sourceArray];
+    const len = arr.length;
+    for(let i = 1; i<len; i++){
+        
+        const temp = arr[i];
+        let j = i;
+        while(j>0 && temp < arr[j - 1]){
+            arr[j] = arr[j - 1];
+            j--
+        }
+        if(j !== i){
+            arr[j] = temp
+        }
+
+    }
+
+    return arr;
+}
+
+
+console.log(InsertSort(arr))
