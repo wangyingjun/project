89224e2f2e27cf46bd3e6f98a9362bac1420acb8
wangyingjun <277933360@qq.com>
2021-05-13 07:15:11 +0800

    SelectionSort

--- code/sort.js
+++ code/sort.js
@@ -29,3 +29,31 @@ const BubbleSort = (sourceArray) => {
 
 let arr = [5,4,2,3,8]
 console.log(BubbleSort(arr))
+
+// 首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置
+// 再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
+// 重复第二步，直到所有元素均排序完毕。
+ 
+const SelectionSort = (sourceArray) => {
+    if(!Array.isArray(sourceArray) || !sourceArray.length) return sourceArray;
+    const arr = [...sourceArray];
+    const len = arr.length;
+    for(let i = 0; i<len; i++){
+        let minIndex = i;
+        for(let j = i+1; j < len; j++){
+            if(arr[j] < arr[minIndex]){
+                minIndex = j;
+            }
+        }
+        if(minIndex !== i){
+            const temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+
+    }
+
+    return arr;
+}
+
+console.log(SelectionSort(arr))
