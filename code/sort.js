4c6704533ba92286d689cba5ccbab3e88aa3d10a
wangyingjun <wangyingjun@mistong.com>
2021-05-13 14:04:03 +0800

    QuickSort

--- code/sort.js
+++ code/sort.js
@@ -127,3 +127,33 @@ const MergeSort = (sourceArray) => {
 
 }
 console.log('MergeSort',MergeSort(arr))
+
+// 快速排序
+// 从数列中挑出一个元素，称为 “基准”（pivot）;
+// 重新排序数列，所有元素比基准值小的摆放在基准前面，所有元素比基准值大的摆在基准的后面（相同的数可以到任一边）。在这个分区退出之后，该基准就处于数列的中间位置。这个称为分区（partition）操作；
+// 递归地（recursive）把小于基准值元素的子数列和大于基准值元素的子数列排序；
+
+const QuickSort = (sourceArray) => {
+    if(!Array.isArray(sourceArray) || !sourceArray.length) return sourceArray;
+    if(sourceArray.length < 1) return sourceArray;
+    const arr = [...sourceArray];
+
+    const middleIndex = Math.floor(arr.length / 2);
+    const pivot = arr[middleIndex];
+    const left = [];
+    const right = [];
+    for(let i = 0; i < arr.length; i++){
+        if(i === middleIndex) continue;
+        if(arr[i]>pivot){
+            right.push(arr[i])
+        }else{
+            left.push(arr[i])
+        }
+    }
+
+    return QuickSort(left).concat(pivot).concat(QuickSort(right))
+
+}
+
+let arr2 = [3,5,8,9,4,1,2,6,7]
+console.log('QuickSort', QuickSort(arr2))
